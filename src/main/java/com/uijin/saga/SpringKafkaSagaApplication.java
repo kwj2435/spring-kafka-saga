44dde158e39968ab2212b1607906bccba55e3e46
김의진(한샘몰백엔드개발팀) <kimuj0035@hanssem.com>
2025-01-20 14:39:47 +0900

    Initial commit

---
+++ src/main/java/com/uijin/saga/SpringKafkaSagaApplication.java
@@ -0,0 +1,13 @@
+package com.uijin.saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSagaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringKafkaSagaApplication.class, args);
+  }
+
+}
